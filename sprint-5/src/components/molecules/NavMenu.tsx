421b46e90090bab306398c59fd19f55217db4df1
David <ruedarosasdavid@gmail.com>
2025-09-17 11:31:38 +0200

    feat: create NavMenu component for desktop

---
+++ sprint-5/src/components/molecules/NavMenu.tsx
@@ -0,0 +1,23 @@
+import type { NavItem } from "../../types";
+
+interface NavMenuProps {
+    items: NavItem[];
+}
+
+const NavMenu = ({ items }: NavMenuProps) => {
+    return (
+        <nav>
+            <ul className="flex space-x-8">
+                {items.map((item) => (
+                    <li key={item.id}>
+                        <a href={item.href} className="text-gray-700 hover:text-red-600 font-medium uppercase text-base">
+                            {item.label}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+};
+
+export default NavMenu;
