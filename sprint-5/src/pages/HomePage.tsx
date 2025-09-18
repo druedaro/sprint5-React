7867e8e3e027cd309af77ccb068cd3e7d927ba03
David <ruedarosasdavid@gmail.com>
2025-09-18 06:22:06 +0200

    feat: create HomePage with all main sections

---
+++ sprint-5/src/pages/HomePage.tsx
@@ -0,0 +1,21 @@
+import Hero from "../components/organisms/Hero";
+import Features from "../components/organisms/Features";
+import Downloads from "../components/organisms/Downloads";
+import Faq from "../components/organisms/Faq";
+import Newsletter from "../components/organisms/Newsletter";
+import SiteFooter from "../components/organisms/SiteFooter";
+
+const HomePage = () => {
+    return (
+        <>
+        <Hero />
+        <Features />
+        <Downloads />
+        <Faq />
+        <Newsletter />
+        <SiteFooter />
+        </>
+    );
+};
+
+export default HomePage;
