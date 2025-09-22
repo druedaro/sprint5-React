1fd1acffde83e3b61e5cc9fbeba2c33a738396c2
David <ruedarosasdavid@gmail.com>
2025-09-22 10:58:37 +0200

    refactor: use atoms Logo and Icon in SiteFooter for consistency

--- src/components/organisms/SiteFooter.tsx
+++ src/components/organisms/SiteFooter.tsx
@@ -1,10 +1,12 @@
+import Logo from '../atoms/Logo';
+import Icon from '../atoms/Icon';
 const SiteFooter = () => {
   return (
     <footer className="bg-gray-900 py-12 px-4">
       <div className="container mx-auto">
         <div className="flex flex-col md:flex-row items-center justify-between space-y-8 md:space-y-0">
           <div className="flex flex-col md:flex-row items-center space-y-6 md:space-y-0 md:space-x-8">
-            <img src="/images/logo-bookmark-white.svg" alt="Bookmark" className="h-8" />
+            <Logo white />
             <nav className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 text-center font-semibold text-base">
               <a href="#features" className="text-gray-300 hover:text-white uppercase tracking-wide transition-colors">
                 Features
@@ -18,11 +20,11 @@ const SiteFooter = () => {
             </nav>
           </div>
           <div className="flex space-x-6">
-            <a href="#" className="text-gray-300 hover:text-white transition-colors">
-              <img src="/images/icon-facebook.svg" alt="Facebook" className="h-6" />
+            <a href="#" className="text-gray-300 hover:text-white transition-colors" aria-label="Facebook">
+              <Icon name="facebook" />
             </a>
-            <a href="#" className="text-gray-300 hover:text-white transition-colors">
-              <img src="/images/icon-twitter.svg" alt="Twitter" className="h-6" />
+            <a href="#" className="text-gray-300 hover:text-white transition-colors" aria-label="Twitter">
+              <Icon name="twitter" />
             </a>
           </div>
         </div>
